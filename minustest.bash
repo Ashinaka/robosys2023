d4aa556c5482eaf8a929f0e9c66861fd65d37de7
Takaki Nakamura <s.jp>
2023-11-29 11:05:07 +0900

    add a command

--- minustest.bash
+++ minustest.bash
@@ -1,4 +1,18 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Takaki Nakamura
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+      echo NG at Line $1
+      res=1
+}
+
+res=0
+
+
 out=$(seq 10 | ./minus)
 
-[ "${out}" = -52 ]
+[ "${out}" = -55 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
